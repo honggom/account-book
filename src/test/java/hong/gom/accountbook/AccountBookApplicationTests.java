42b4ff41da2ad92a79838f48b468df259cb9d997
honggom <cavok699@naver.com>
2022-02-18 10:32:32 +0900

    init

---
+++ src/test/java/hong/gom/accountbook/AccountBookApplicationTests.java
@@ -0,0 +1,13 @@
+package hong.gom.accountbook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccountBookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
